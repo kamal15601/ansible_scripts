c1b7298d8e58bf39e766d7d492591471dc04e1df
Cloud User <ec2-user@ip-10-0-0-10.ec2.internal>
2026-01-21 11:57:36 +0000

    updated-21/01

--- ansible_sc/register_subs_mn.sh
+++ ansible_sc/register_subs_mn.sh
@@ -3,6 +3,18 @@ echo " this script is used to register redhat subscription"
 username="$1"
 password="$2"
 
+if [[ $# -lt 2 ]]
+then
+    echo "please provide username and password as arguments to proceed further"
+    exit 1
+fi
+
+if [[ $(id -u) -ne 0 ]]
+then
+    echo "please run this script as root"
+    exit 1
+fi
+
 
 subscription-manager register --username="$username" --password="$password"
 
